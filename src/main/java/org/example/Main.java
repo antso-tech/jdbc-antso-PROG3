26cc6153b8793d7e75af16285ba3b14d3587d17c
antso.tech <hei.antso.2007@gmail.com>
2025-12-04 10:50:56 +0300

    test avec offset

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,14 +9,14 @@ public class Main {
     String user = "postgres";
     String password = "ntsoa";
 
-    String query = "SELECT * FROM users WHERE name ILIKE ? LIMIT ? ";
+    String query = "SELECT * FROM users WHERE name ILIKE ? LIMIT ? OFFSET ?";
 
     public void getUserByPagination(String word, Integer limit, Integer offset){
         try(Connection conn = DriverManager.getConnection(url, user, password);
         PreparedStatement pst = conn.prepareStatement(query)){
             pst.setString(1,"%" + word +"%" );
             pst.setInt(2,limit);
-
+            pst.setInt(3,offset);
 
             ResultSet rs = pst.executeQuery();
 
@@ -36,6 +36,6 @@ public class Main {
     }
     public static void main(String[] args) {
         Main test = new Main();
-        test.getUserByPagination("al",2,2);
+        test.getUserByPagination("a",2,3);
     }
 }

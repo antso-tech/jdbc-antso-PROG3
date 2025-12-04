4e56b78587ff9515b9ef52b0e62f131595e9941e
antso.tech <hei.antso.2007@gmail.com>
2025-12-04 10:45:31 +0300

    test d"utilisation de ilike

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    String url = "jdbc:postgresql://localhost/users";
+    String user = "postgres";
+    String password = "ntsoa";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    String query = "SELECT * FROM users WHERE name ILIKE ?";
+
+    public void getUserByPagination(String word, Integer limit, Integer offset){
+        try(Connection conn = DriverManager.getConnection(url, user, password);
+        PreparedStatement pst = conn.prepareStatement(query)){
+            pst.setString(1,"%" + word +"%" );
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()){
+                String name = rs.getString("name");
+                String firstname = rs.getString("firstname");
+                String email = rs.getString("email");
+                System.out.println(name + "|" + firstname + "|" + email);
+            }
+
+
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+
         }
+
+    }
+    public static void main(String[] args) {
+        Main test = new Main();
+        test.getUserByPagination("al",10,2);
     }
 }
